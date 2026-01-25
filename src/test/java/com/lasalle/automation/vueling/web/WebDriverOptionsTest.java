e7fb2237b4bbb97dda879313e0820be7191b7755
Sacristan,Sergio (IT CTO) BI-ES-S <sergio.sacristan@boehringer-ingelheim.com>
2026-01-25 22:17:04 +0100

    update versions 2026

--- src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
+++ src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
@@ -1,13 +1,14 @@
 package com.lasalle.automation.vueling.web;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.lang.invoke.MethodHandles;
 
 /**
@@ -19,7 +20,7 @@ import java.lang.invoke.MethodHandles;
  * Locators según preferencia:
  * By Id
  * By name
- * By css: <a href="https://saucelabs.com/resources/articles/selenium-tips-css-selectors">...</a>
+ * By css: <a href="https://saucelabs.com/resources/articles/selenium-tips-css-selectors">selenium-tips-css-selectors</a>
  * By xpath
  * Wait: implicitlyWait vs explicitWait (expected conditions)
  */
@@ -31,11 +32,7 @@ public class WebDriverOptionsTest {
     @BeforeEach
     public void setUp() {
         LOGGER.debug("start testWebDrive");
-        // TODO download from https://www.selenium.dev/ecosystem/
-        // https://googlechromelabs.github.io/chrome-for-testing/
-        // TODO update System.setProperty
-        System.setProperty ("webdriver.chrome.driver", "absolute path to webdriver" );
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
     }
 
     @AfterEach
@@ -45,11 +42,27 @@ public class WebDriverOptionsTest {
     }
 
     @Test
-    public void testWebDrives()
-    {
+    public void testWebDrives() {
+        LOGGER.debug("Start testWebDrives");
         driver.manage().window().maximize();
         LOGGER.debug("driver starter");
-        Assertions.assertNotNull(driver);
+        driver.navigate().to("https://www.selenium.dev/ecosystem");
+        Assertions.assertThat(driver).isNotNull();
+        LOGGER.debug("Finish testWebDrives");
     }
 
+    @Test
+    public void testWebDriveNavigation() {
+        LOGGER.debug("Start testWebDriveNavigation");
+        // TODO go to url https://the-internet.herokuapp.com
+        // TODO get the title
+        // TODO get the current url
+        // TODO get the page source
+        // TODO navigate to https://the-internet.herokuapp.com/abtest
+        // TODO navigate back
+        // TODO navigate forward
+        // TODO navigate refresh
+        // TODO assert the current url is https://the-internet.herokuapp.com/abtest
+        LOGGER.debug("Finish testWebDriveNavigation");
+    }
 }
